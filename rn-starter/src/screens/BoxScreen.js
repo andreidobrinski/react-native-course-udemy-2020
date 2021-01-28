21c5e845950685dc5e184d1df89cd96a04a0f75e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-28 17:32:23 -0500

    flex layout

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
+const BoxScreenLayout = () => {
+  return (
+    <View style={stylesLayout.parentStyle}>
+      <View style={stylesLayout.viewOneStyle} />
+      <View style={stylesLayout.viewTwoStyle} />
+      <View style={stylesLayout.viewThreeStyle} />
+    </View>
+  );
+};
+
+const stylesLayout = StyleSheet.create({
+  parentStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red',
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
+    top: 50,
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple',
+  }
+});
+
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
@@ -18,6 +54,7 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    height: 200,
   },
   textStyle: {
     borderWidth: 3,
