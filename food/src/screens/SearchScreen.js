b4696880106c4e7430f6eac5f155fd0c32b1b7e7
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-29 16:49:07 -0500

    add param to searchApi()

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'toronto'
         }
       });
@@ -28,7 +28,7 @@ const SearchScreen = () => {
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>Results length: {results.length}</Text>
