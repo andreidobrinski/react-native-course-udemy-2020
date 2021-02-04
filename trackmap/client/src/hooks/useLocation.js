a8dd81d778198a599bc454be5441aad54f8788d5
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-04 15:56:11 -0500

    update useLocation to stop tracking when navigating away from screen

--- trackmap/client/src/hooks/useLocation.js
+++ trackmap/client/src/hooks/useLocation.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-location';
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
 
   const startWatching = async () => {
     try {
       const { granted } = await requestPermissionsAsync();
-      await watchPositionAsync({
+      const sub = await watchPositionAsync({
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10,
@@ -17,14 +18,20 @@ export default (callback) => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
+      setSubscriber(sub);
     } catch (e) {
       setErr(e);
     }
   };
 
   useEffect(() => {
-    startWatching()
-  }, []);
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      subscriber.remove();
+      setSubscriber(null);
+    }
+  }, [shouldTrack]);
 
   return [err];
 };
